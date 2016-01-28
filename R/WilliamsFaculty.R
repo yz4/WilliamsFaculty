642aa47dee8e0161a536ca5123c9505063647b9a
Harry Zhang <yz4@williams.edu>
2016-01-27 22:28:41 -0800

    Created repo

---
+++ R/WilliamsFaculty.R
@@ -0,0 +1,7 @@
+#' Calculates the mean of the ages of all Williams faculty
+#' 
+#' @return The average age of all Williams faculty
+average_age <- function() {
+  mean(data$Age)
+}
+
---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Ages of all Williams faculty 
+#' 
+#' A dataset containing the department, graduation year, and age of all 403 Williams
+#' faculty, including visiting lecturers
+#' 
+#' @format A data frame with 403 rows and 4 variables:
+#' \describe{
+#'    \item{name}{the professor's name}
+#'    \item{department}{the professor's department}
+#'    \item{year}{the year the professor graudated college}
+#'    \item{age}{the age of the professor, in years}
+#'  }
+#'  @source \url{http://web.williams.edu/admin/registrar//catalog/archive.html}
+"data"
+#> [1] "data"

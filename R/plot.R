968fb11ad977a54b77c41e49e7429f16432929a9
Harry Zhang <yz4@williams.edu>
2016-01-30 14:31:12 -0500

    ss

--- R/plot.R
+++ R/plot.R
@@ -1,5 +1,7 @@
 #' Plots the age distribution of all Williams faculty
 #' 
+#' @export
 plot_age_distribution <- function(){
+  data(age)
   hist(age$Age, main = "Age Distribution of Williams Faculty", xlab = "Age (by years)")
 }

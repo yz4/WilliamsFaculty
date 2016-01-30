826eea306ffa3d491e75a80ffc7cb0ca9c7c8540
Harry Zhang <yz4@williams.edu>
2016-01-30 14:14:17 -0500

    down version req

---
+++ R/summary.R
@@ -0,0 +1,11 @@
+#' Generates a summary of relevant data
+#' 
+#' @return summary of relevant data
+age_summary <- function(){
+  attributes <- c("Number of Professors","Range of Ages", "Oldest", "Youngest", "Average Age",
+                  "Sd", "Variance")
+  attribute_data <- with(data, c(nrow(data),max(Age)-min(Age),Name[match(max(Age),Age)],
+                  Name[match(min(Age),Age)], mean(Age),sd(Age),var(Age)))
+  df <- data.frame(attributes,attribute_data)
+  head(df)
+}
